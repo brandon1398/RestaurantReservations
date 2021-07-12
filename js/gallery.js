411fca55292fd297ca896d91abdb7b50003396a3
brandon1398 <af465411@gmail.com>
2021-07-12 16:09:14 -0500

    Index

---
+++ js/gallery.js
@@ -0,0 +1,38 @@
+window.addEventListener('load', function(){
+	new Glider(document.querySelector('.carousel__lista'), {
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		dots: '.carousel__indicadores',
+		arrows: {
+			prev: '.carousel__anterior',
+			next: '.carousel__siguiente'
+		},
+		responsive: [
+			{
+			  breakpoint: 450,
+			  settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1
+			  }
+			},{
+			  breakpoint: 570,
+			  settings: {
+				slidesToShow: 2,
+				slidesToScroll: 2
+			  }
+			},{
+			  breakpoint: 850,
+			  settings: {
+				slidesToShow: 3,
+				slidesToScroll: 3
+			  }
+			},{
+			  breakpoint: 1000,
+			  settings: {
+				slidesToShow: 4,
+				slidesToScroll: 4
+			  }
+			}
+		]
+	});
+});
